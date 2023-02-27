3efecd8a9256ce789097402dec3202910e335a91
2020030275 <2020030275@upsin.edu.mx>
2023-02-27 16:40:20 -0700

    Archivo app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require("http")
+const express = require("express")
+const ejs = require('ejs')
+const bodyparser = require('body-parser')
+const router = require('./router/index')
+const path = require('path')
+const port = 500
+
+const app = express()
+app.set('view engine',"ejs")
+app.use(express.static(__dirname +'/public'))
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(router)
+app.engine('html', require('ejs').renderFile)
+
+
+app.listen(port, () => {
+    console.log("Iniciando puerto 500")
+})
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(__dirname + "/public/error.html")
+})
