88e7957206eff01695ba89837fcd6466283908f8
2020030275 <2020030275@upsin.edu.mx>
2023-03-03 12:14:17 -0700

    Cambiar los archivos a html

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ app.use(router)
 app.engine('html', require('ejs').renderFile)
 
 
-app.listen(port, () => {
-    console.log("Iniciando puerto 500")
-})
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(__dirname + "/public/error.html")
+})
+
+app.listen(port, () => {
+    console.log("Iniciando puerto 500")
 })
--- router/index.js
+++ router/index.js
@@ -3,28 +3,41 @@ const router = express.Router()
 const ejs = require('ejs');
 const bodyparser = require('body-parser')
 
-const port = 500
 
-
-
-router.get("/", (req, res) => {
+router.get('/index', (req, res) => {
 	const valores = {
 		
+		numBoleto: req.query.numBoleto,
+		destino: req.query.destino,
+		nombreDelCliente: req.query.nombreDelCliente,
+		anosCumplidos: req.query.anosCumplidos,
 		tipoDeViaje: req.query.tipoDeViaje,
 		precio: req.query.precio,
+		subtotal: req.query.subtotal,
+        impuesto: req.query.impuesto,
+        descuento: req.query.descuento,
+        totalAPagar: req.query.totalAPagar
 	};
-	res.render("index.ejs", valores);
+	res.render('index.html', valores);
 });
 
 
 
-router.post("/", (req, res) => {
+router.post('/index', (req, res) => {
 	const valores = {
-		
+		subtotal: req.body.subtotal,
+		numBoleto: req.body.numBoleto,
+		destino: req.body.destino,
+		nombreDelCliente: req.body.nombreDelCliente,
+		anosCumplidos: req.body.anosCumplidos,
 		tipoDeViaje: req.body.tipoDeViaje,
 		precio: req.body.precio,
+		subtotal: req.body.subtotal,
+        impuesto: req.body.impuesto,
+        descuento: req.body.descuento,
+        totalAPagar: req.body.totalAPagar
 	};
-	res.render("index", valores);
+	res.render('index.html', valores);
 });
 
 
